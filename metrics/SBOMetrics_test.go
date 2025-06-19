8bbf555d7c60b414b2db4586ad4e43a8c74aeec3
Serkan Özkan <serkan@sbosoft.net>
2025-06-19 11:41:29 +0300

    Fix compile error (but it's not a real test)

--- metrics/SBOMetrics_test.go
+++ metrics/SBOMetrics_test.go
@@ -8,7 +8,8 @@ import (
 
 func TestSBOMetricTimeWindow(t *testing.T) {
 	timest := time.Date(2025, 11, 17, 20, 34, 58, 651387237, time.UTC)
-	sbom := NewSBOMetric(SBO_METRIC_REQ_COUNT, "aaa")
+	metricManager := NewSBOMetricsManager()
+	sbom := NewSBOMetric(SBO_METRIC_REQ_COUNT, "aaa", metricManager)
 	sbom.addValue("unittest", timest, 100)
 
 	slog.Warn("Values", "values", sbom.Values)
